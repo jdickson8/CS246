aaa00731832089666db7d80d6f8c20c199440ef6
jdickson8 <johnslash8@gmail.com>
2015-10-09 11:56:49 -0600

    Added github to hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 //This is a comment for a commit sake
 public class Bob {
     public static void main(String[] args) {
-    	System.out.println("Hello World");
+    	System.out.println("Hello GitHub World");
     }
 }
